48a6e4341af2fc3787e989cd7a0d5107f7c06c18
Aaron Tan <atan8379@gmail.com>
2025-04-21 17:46:20 -0700

    Favicon Test
    First try at attempting to add a favicon

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import { Montserrat, Asap } from 'next/font/google';
 import PageTransition from '../src/components/global/PageTransition';
 import Layout from '../src/components/global/Layout';
 
+
 export const montserrat = Montserrat({
     subsets: ['latin'],
     display: 'swap',
@@ -17,6 +18,19 @@ export const asap = Asap({
 export default function MyApp({ Component, pageProps, router }) {
     return (
         <main className={asap.variable}>
+            <link rel="icon" href="/favicon.ico" sizes="any" />
+            <link
+                rel="icon"
+                href="/icon?<generated>"
+                type="image/<generated>"
+                sizes="<generated>"
+            />
+            <link
+                rel="apple-touch-icon"
+                href="/apple-icon?<generated>"
+                type="image/<generated>"
+                sizes="<generated>"
+            />
             <Layout>
                 <PageTransition routeKey={router.pathname}>
                     <Component {...pageProps} />
