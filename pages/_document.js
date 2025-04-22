1b35947751bd83ceb0deac66e5827bd752aaadee
Aaron Tan <atan8379@gmail.com>
2025-04-21 18:03:01 -0700

    3rd try at doing Favicons
    3rd times the charm, amirite?

--- pages/_document.js
+++ pages/_document.js
@@ -3,20 +3,12 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
     return (
         <Html lang="en">
-            <link rel="icon" href="/favicon.ico" sizes="any" />
-            <link
-                rel="icon"
-                href="/icon?<generated>"
-                type="image/<generated>"
-                sizes="<generated>"
-            />
-            <link
-                rel="apple-touch-icon"
-                href="/apple-icon?<generated>"
-                type="image/<generated>"
-                sizes="<generated>"
-            />
-            <Head />
+            <Head>
+                <link rel="icon" href="/favicon.ico" sizes="any" />
+                <link rel="icon" type="image/png" href="/icon.png" sizes="32x32" />
+                <link rel="apple-touch-icon" href="/apple-icon.png" sizes="180x180" />
+                <link rel="manifest" href="/site.webmanifest" />
+            </Head>
             <body style={{ backgroundColor: '#14111D' }}>
                 <Main />
                 <NextScript />
